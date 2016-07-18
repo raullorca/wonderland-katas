3827e50cbea702521c1489faaaa029e00ce45740
lorcaraul <lorca.raul@gmail.com>
2016-07-18 19:04:11 +0200

    Agregar archivos de proyecto.

---
+++ wonderland-katas/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace wonderland_katas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
