a6e61910ed58e0d410a333ec8cb2742ffe4de938
lorcaraul <lorca.raul@gmail.com>
2016-07-18 20:27:31 +0200

    funciona en bruto

---
+++ wonderland-katas/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wonderland_katas
+{
+    class Class1
+    {
+
+        public void xxxx()
+        {
+            var ooo = new int[]{ 10, 15, 20, 27, 35, 76 };
+
+            var jj = new int[]{ 20,30,40,54,70,152};
+
+            var yyy = new int[] { 2, 3, 4, 5, 6 };
+
+        }
+    }
+}
