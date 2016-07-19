222d4c408d17d0b1ff5a24fa167c42f4a74cd9ba
lorcaraul <lorca.raul@gmail.com>
2016-07-19 23:20:25 +0200

    Programa compleado con fuerza bruta

---
+++ wonderland-katas/Wonderland.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace wonderland_katas
+{
+    public class Wonderland
+    {
+        public static bool IsMagicNumber(int number)
+        {
+            var divisorList = new List<int> { 2, 3, 4, 5, 6 };
+            bool isMagic = true;
+            foreach (var item in divisorList)
+            {
+                if (!IsMagicNumber(number, item))
+                {
+                    isMagic = false;
+                    break;
+                }
+            }
+            return isMagic;
+        }
+        internal static bool IsMagicNumber(int number, int divisor)
+        {
+            var arrayOrigen = GenerateArray(number);
+            var resultado = number * divisor;
+            var arrayDestino = GenerateArray(resultado);
+            return CompararNumeros(arrayOrigen, arrayDestino);
+        }
+
+        public static int[] GenerateArray(int number)
+        {
+            var texto = number.ToString().ToCharArray();
+            var array = texto.Select(x => Convert.ToInt32(x.ToString())).ToArray();
+            return array;
+        }
+
+        public static bool CompararNumeros(int[] arrayOrigen, int[] arrayDestino)
+        {
+            var origenOrdenado = arrayOrigen.OrderBy(x => x).ToArray();
+            var destinoOrdenado = arrayDestino.OrderBy(x => x).ToArray();
+
+
+            for (int i = 0; i < origenOrdenado.Count(); i++)
+            {
+                if (origenOrdenado[i] != destinoOrdenado[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ wonderland-katas/WonderlandNumberTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace wonderland_katas
+{
+    [TestClass]
+    public class WonderlandNumberTests
+    {
+        [TestMethod]
+        public void IsMagicNumber()
+        {
+            var magicNumbers = new List<int>();
+            for (int number = 100000; number <= 999999; number++)
+            {
+                if (Wonderland.IsMagicNumber(number))
+                    magicNumbers.Add(number);
+            }
+
+            Assert.IsTrue(magicNumbers.Any());
+            Assert.AreEqual(1, magicNumbers.Count);
+            Assert.AreEqual(142857, magicNumbers[0]);
+        }
+
+
+
+        [TestMethod]
+        public void IsMagicNumber_ByNumber()
+        {
+            var items = new int[,]
+            {
+                { 2, 125874 },
+                { 3, 100035 },
+                { 4, 102564 },
+                { 5, 142857 },
+                { 6, 106848 }
+            };
+
+            for (int i = 0; i <= items.GetLength(0) - 1; i++)
+            {
+                var result = Wonderland.IsMagicNumber(items[i, 1], items[i, 0]);
+                Assert.IsTrue(result);
+            }
+
+        }
+
+        [TestMethod]
+        public void CompararNumeros()
+        {
+            var origen = new int[] { 1, 2, 3, 4, 5, 6 };
+            var destino = new int[] { 6, 5, 4, 3, 2, 1 };
+            var resultado = Wonderland.CompararNumeros(origen, destino);
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void GenerateArray()
+        {
+            var origen = 123456;
+            var esperado = new int[] { 1, 2, 3, 4, 5, 6 };
+            var resultado = Wonderland.GenerateArray(origen);
+            Assert.AreEqual(resultado[0], esperado[0]);
+            Assert.AreEqual(resultado[1], esperado[1]);
+            Assert.AreEqual(resultado[2], esperado[2]);
+            Assert.AreEqual(resultado[3], esperado[3]);
+            Assert.AreEqual(resultado[4], esperado[4]);
+            Assert.AreEqual(resultado[5], esperado[5]);
+        }
+    }
+}
